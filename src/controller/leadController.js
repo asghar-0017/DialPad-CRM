7e2131cfb2ff18ee51418ec8b25aeaaf1960e534
asghar-0017 <asghar.usman0017@gmail.com>
2024-08-06 21:19:08 +0500

    changing

--- src/controller/leadController.js
+++ src/controller/leadController.js
@@ -13,7 +13,6 @@ const leadController = {
               return res.status(400).json({ message: 'followUpDetail is required when customer_feedBack is followUp' });
             }
           }
-
           const lead = await leadService.leadCreateService(data);
           res.status(201).json({ message: 'Lead created successfully', lead });
         } catch (error) {
