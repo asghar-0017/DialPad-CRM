276edda3450092696b3e641b005bd7f20d801815
asghar-0017 <asghar.usman0017@gmail.com>
2024-08-20 17:58:19 +0500

    whatsApp

--- src/routes/messagingRoute.js
+++ src/routes/messagingRoute.js
@@ -3,11 +3,10 @@ const combinedAuthenticate=require('../middleware/permission')
 const {checkRole}=require('../middleware/checkRole')
 
 const messageRoute = (app,io) => {
-    app.post('/send-message-to-agent/:agentId',combinedAuthenticate, checkRole(['admin','agent']),(req,res)=> messageController.sendMessageToAgent(io,req,res));
+    app.post('/send-message-to-agent/:agentId',combinedAuthenticate, checkRole(['admin']),(req,res)=> messageController.sendMessageToAgent(io,req,res));
     app.get('/get-send-message-to-agent/:agentId',combinedAuthenticate, checkRole(['admin','agent']),(req,res)=> messageController.getAssignMessagesById(io,req,res));
-    app.post('/send-message-to-admin/:adminId/:agentId',combinedAuthenticate, checkRole(['admin','agent']),(req,res)=> messageController.sendMessageToAdmin(io,req,res));
+    app.post('/send-message-to-admin/:adminId/:agentId',combinedAuthenticate, checkRole(['agent']),(req,res)=> messageController.sendMessageToAdmin(io,req,res));
     app.get('/get-send-message-to-admin/:adminId/:agentId',combinedAuthenticate, checkRole(['admin','agent']),(req,res)=> messageController.getSendMessagesToAdminById(io,req,res));
-
     app.get('/get-all-messages-to-admin/:adminId', combinedAuthenticate, checkRole(['admin']), (req, res) => messageController.getAllMessagesFromAdmin(io,req, res));
 
 
