675c3720160f40b9166eea958b40add6f4d11d1c
asghar-0017 <asghar.usman0017@gmail.com>
2024-09-03 00:52:43 +0500

    vercel

--- src/infrastructure/psql.js
+++ src/infrastructure/psql.js
@@ -7,7 +7,7 @@ const { PinoLogger } = require('../../logger');
 const dataSource = new DataSource({
   type: "postgres",
   host: process.env.POSTGRES_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   username: process.env.POSTGRES_USER,
   password: "SoftMark#2024$",
   database: process.env.POSTGRES_DB,
@@ -15,10 +15,12 @@ const dataSource = new DataSource({
   logging: true,
   logger: new PinoLogger(),
   entities: [
+    path.join(__dirname, "../entities/tempAgent.js"),
+    path.join(__dirname, "../entities/agentMessage.js"),
+    // Add each entity explicitly if needed
     path.join(__dirname, "../entities/**/*.js"),
-    path.join(__dirname, "../entities/trash/**/*.js"), // Include trash entities here
+    path.join(__dirname, "../entities/trash/**/*.js"),
   ],
 });
-
 
 module.exports = dataSource;
