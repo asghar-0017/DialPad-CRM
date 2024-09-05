8ec96d8e5acbc6d63ce0eeb818284c5118f42c9c
asghar-0017 <asghar.usman0017@gmail.com>
2024-09-05 17:48:41 +0500

    did task work

--- src/infrastructure/psql.js
+++ src/infrastructure/psql.js
@@ -10,31 +10,31 @@ const { PinoLogger } = require('../../logger');
 const entityPath = path.join(__dirname, "..", "entities", "*.js");
 console.log("Loading entities from:", entityPath);
 
-// const dataSource = new DataSource({
-//   type: "postgres",
-//   host: process.env.POSTGRES_HOST,
-//   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
-//   username: process.env.POSTGRES_USER,
-//   password: "SoftMark#2024$", // Use env variable
-//   database: process.env.POSTGRES_DB,
-//   synchronize: true,
-//   logging: true,
-//   logger: new PinoLogger(),
-//   entities: [entityPath],
-// });
-
-
 const dataSource = new DataSource({
   type: "postgres",
-  host: 'localhost',
-  port:  5432,
-  username: 'postgres',
-  password: "postgres", 
-  database: 'postgres',
-  synchronize: true,  // Set this to false
+  host: process.env.POSTGRES_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  username: process.env.POSTGRES_USER,
+  password: "SoftMark#2024$", // Use env variable
+  database: process.env.POSTGRES_DB,
+  synchronize: true,
   logging: true,
   logger: new PinoLogger(),
   entities: [entityPath],
 });
+
+
+// const dataSource = new DataSource({
+//   type: "postgres",
+//   host: 'localhost',
+//   port:  5432,
+//   username: 'postgres',
+//   password: "postgres", 
+//   database: 'postgres',
+//   synchronize: true,  // Set this to false
+//   logging: true,
+//   logger: new PinoLogger(),
+//   entities: [entityPath],
+// });
 
 module.exports = dataSource;
