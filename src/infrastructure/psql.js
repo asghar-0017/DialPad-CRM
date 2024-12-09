be61d947bbdb5d4ae444d760fd72da33cc65e321
asghar-0017 <asghar.usman0017@gmail.com>
2024-12-09 22:44:45 +0500

    vercel deployee

--- src/infrastructure/psql.js
+++ src/infrastructure/psql.js
@@ -1,49 +1,74 @@
-const { DataSource } = require("typeorm");
-const path = require("path");
-const dotenv = require("dotenv");
-dotenv.config();
-const { PinoLogger } = require('../../logger');
+// const { DataSource } = require("typeorm");
+// const path = require("path");
+// const dotenv = require("dotenv");
+// dotenv.config();
+// const { PinoLogger } = require('../../logger');
 
-const entityPath = path.join(__dirname, "..", "entities", "*.js");
+// const entityPath = path.join(__dirname, "..", "entities", "*.js");
 
-const dataSource = new DataSource({
-  type: "postgres",
-  host: 'localhost',
-  port:  5432,
-  username: 'postgres',
-  password: "postgres", 
-  database: 'postgres',
-  synchronize: true,  // Set this to false
-  logging: true,
-  logger: new PinoLogger(),
-  entities: [entityPath],
-});
 // const dataSource = new DataSource({
+// //   type: "postgres",
+// //   host: 'localhost',
+// //   port:  5432,
+// //   username: 'postgres',
+// //   password: "postgres", 
+// //   database: 'postgres',
+// //   synchronize: true,  // Set this to false
+// //   logging: true,
+// //   logger: new PinoLogger(),
+// //   entities: [entityPath],
+// // });
+// // const dataSource = new DataSource({
 //   type: "postgres",
-//   host: process.env.POSTGRES_HOST,
-//   port:  process.env.DB_PORT,
-//   username: process.env.POSTGRES_USER,
-//   password: 'SoftMark#2024$', 
-//   database: process.env.POSTGRES_DB,
+//   host: 'pg-c8f8020-maju-31c9.f.aivencloud.com',
+//   port:  '28730',
+//   username: 'avnadmin',
+//   password: 'AVNS_MwtQUtP9nagQvFYuQQ2', 
+//   database: 'defaultdb',
 //   synchronize: true,  // Set this to false
 //   logging: true,
 //   logger: new PinoLogger(),
 //   entities: [entityPath],
 // });
 
-// const dataSource = new DataSource({
-//   type: "postgres",
-//   host: "junction.proxy.rlwy.net",  // Public host
-//   port: 37929,                      // Port from the public URL
-//   username: 'postgres',             // Username
-//   password: "HSUELWSKYTKHeWNyEzeToxgcvhEDGLUz",  // Password
-//   database: 'railway',              // Database name
-//   synchronize: true,                // or false in production
-//   logging: true,
-//   logger: new PinoLogger(),
-//   entities: [entityPath],
-// });
+// // const dataSource = new DataSource({
+// //   type: "postgres",
+// //   host: "junction.proxy.rlwy.net",  // Public host
+// //   port: 37929,                      // Port from the public URL
+// //   username: 'postgres',             // Username
+// //   password: "HSUELWSKYTKHeWNyEzeToxgcvhEDGLUz",  // Password
+// //   database: 'railway',              // Database name
+// //   synchronize: true,                // or false in production
+// //   logging: true,
+// //   logger: new PinoLogger(),
+// //   entities: [entityPath],
+// // });
 
 
+
+// module.exports = dataSource;
+const { DataSource } = require("typeorm");
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config();
+const { PinoLogger } = require("../../logger");
+
+// Extract connection details from the connection string
+const DATABASE_URL = process.env.DATABASE_URL || "postgres://avnadmin:AVNS_MwtQUtP9nagQvFYuQQ2@pg-c8f8020-maju-31c9.f.aivencloud.com:28730/defaultdb?sslmode=require";
+const url = new URL(DATABASE_URL);
+
+const dataSource = new DataSource({
+  type: "postgres",
+  host: url.hostname,
+  port: parseInt(url.port, 10),
+  username: url.username,
+  password: url.password,
+  database: url.pathname.split("/")[1],
+  synchronize: true, // Use false in production
+  logging: true,
+  ssl: url.searchParams.get("sslmode") === "require" ? { rejectUnauthorized: false } : false,
+  logger: new PinoLogger(),
+  entities: [path.join(__dirname, "..", "entities", "*.js")],
+});
 
 module.exports = dataSource;
